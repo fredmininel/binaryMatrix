774a34183fb22c1e8650be61b2dc7dab4dcecce0
Fred Mininel <fredmininel@hotmail.com>
2021-04-05 00:22:26 -0300

    add method to find maxArea in biggest rectangle

--- src/matrixApp.java
+++ src/matrixApp.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class matrixApp {
 
     private static int generateRandomNumber(int limit) {
@@ -63,9 +64,80 @@ public class matrixApp {
             System.out.println("],");
         }
     }
+
+    private static int calculateMaxArea(int[] row) {
+        int maxArea = 0;
+        int maxElem = 0;
+        int area = 0;
+        int height = 0;
+        int width = 1;
+        
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] != 0) {
+                if ((i+1) < row.length) {
+                    height = row[i];
+                    area = height * width;
+                    maxArea = Math.max(maxArea, area);
+                    for (int j = i + 1; j < row.length; j++) {
+                        if (row[j] != 0) {
+                            height = Math.min(height, row[j]);
+                            width += 1;
+                            if (((j+1) < row.length) && (row[j+1] < row[j])) {
+                                area = height * width;
+                                maxArea = Math.max(maxArea, area);
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    height = row[i];
+                    width = 1;
+                }
+            }
+            area = height * width;
+            maxArea = Math.max(maxArea, area);
+            width = 1;
+        }
+
+        return maxArea;
+    }
+    
+    private static void findRectArea(int[][] matrix) {
+        int area = 0;
+        int horizontal = 0;
+        int horizontalLast = 0;
+        int horizontalAux = 0;
+        int vertical = 1;
+        int maxArea = 0;
+        int[][] matrixA = {{1, 1, 1, 1, 1},
+                          {0, 1, 1, 1, 0},
+                          {1, 1, 1, 1, 1},
+                          {0, 0, 0, 0, 0},
+                          {0, 0, 1, 0, 1},
+                         };
+
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i != 0) {
+                    if (matrix[i][j] == 1) {
+                        matrix[i][j] += matrix[i-1][j];
+                    }
+                }
+            }   
+            maxArea = Math.max(maxArea, calculateMaxArea(matrix[i]));
+        }
+        System.out.println("result: ");
+        printMatrix(matrix);
+        System.out.println("MaxArea: " + maxArea);
+    }
+
     public static void main(String[] args) throws Exception {
         int matrix[][] = createMatrix();
         matrix = populateMatrix(matrix);
         printMatrix(matrix);
+        findRectArea(matrix);
     }
 }
